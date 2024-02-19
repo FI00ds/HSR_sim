467fae7d9c689f25199656b8bfb61c3a5da5f8d9
FI00ds <lucasrobertson368@gmail.com>
2024-02-19 13:06:10 +0100

    trace formatting

---
+++ traces.h
@@ -0,0 +1,108 @@
+struct stat_boost{
+    char* type;
+    float amount;
+};
+
+struct traces{
+    int id;
+    struct stat_boost trace1;
+    struct stat_boost trace2;
+    struct stat_boost trace3;
+    struct stat_boost trace4;
+    struct stat_boost trace5;
+    struct stat_boost trace6;
+    struct stat_boost trace7;
+    struct stat_boost trace8;
+    struct stat_boost trace9;
+    struct stat_boost trace10;
+};
+
+struct traces trid8004 = {};
+
+struct traces trid8004 = {};
+
+struct traces trid1211 = {};
+
+struct traces trid1209 = {};
+
+struct traces trid1206 = {};
+
+struct traces trid1202 = {};
+
+struct traces trid1201 = {};
+
+struct traces trid1109 = {};
+
+struct traces trid1108 = {};
+
+struct traces trid1107 = {};
+
+struct traces trid1106 = {};
+
+struct traces trid1105 = {};
+
+struct traces trid1104 = {};
+
+struct traces trid1103 = {};
+
+struct traces trid1102 = {};
+
+struct traces trid1101 = {};
+
+struct traces trid1013 = {};
+
+struct traces trid1009 = {};
+
+struct traces trid1008 = {};
+
+struct traces trid1004 = {};
+
+struct traces trid1003 = {};
+
+struct traces trid1002 = {};
+
+struct traces trid1001 = {};
+
+struct traces trid1204 = {};
+
+struct traces trid1006 = {};
+
+struct traces trid1207 = {};
+
+struct traces trid1203 = {};
+
+struct traces trid1205 = {};
+
+struct traces trid1111 = {};
+
+struct traces trid1005 = {};
+
+struct traces trid1213 = {};
+
+struct traces trid1208 = {};
+
+struct traces trid1110 = {};
+
+struct traces trid1212 = {};
+
+struct traces trid1210 = {};
+
+struct traces trid1112 = {};
+
+struct traces trid1217 = {};
+
+struct traces trid1302 = {};
+
+struct traces trid1215 = {};
+
+struct traces trid1303 = {};
+
+struct traces trid1214 = {};
+
+struct traces trid1305 = {};
+
+struct traces trid1307 = {};
+
+struct traces trid1306 = {};
+
+struct traces trid1312 = {};
