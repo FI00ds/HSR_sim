e8685ad9f5cf58b93dcb74be16f72af5eebb971d
FI00ds <lucasrobertson368@gmail.com>
2024-02-22 10:24:18 +0100

    added name, id, path, and rarity for all 3 and 4 star lightcones

---
+++ lightconestats.h
@@ -0,0 +1,197 @@
+struct lightconestats{
+    char* name;
+    int path;
+    int id;
+    int HP;
+    int ATK;
+    int DEF;
+    int rarity;
+};
+
+//Lightcone stats are for lvl 80 lightcones
+
+struct lightconestats lcid20000 = {"Arrows", 3, 20000, 846, 317, 264, 3};
+
+struct lightconestats lcid20001 = {"Cornucopia", 7, 20001, 952, 264, 264, 3};
+
+struct lightconestats lcid20002 = {"Collapsing Sky", 1, 20002, , , , 3};
+
+struct lightconestats lcid20003 = {"Amber", 6, 20003, , , , 3};
+
+struct lightconestats lcid20004 = {"void", 4, 20004, , , , 3};
+
+struct lightconestats lcid20005 = {"Chorus", 5, 20005, , , , 3};
+
+struct lightconestats lcid20006 = {"Data Bank", 2, 20006, , , , 3};
+
+struct lightconestats lcid20007 = {"Darting Arrows", 3, 20007, , , , 3};
+
+struct lightconestats lcid20008 = {"Fine Fruit", 7, 20008, , , , 3};
+
+struct lightconestats lcid20009 = {"Shattered Home", 1, 20009, , , , 3};
+
+struct lightconestats lcid20010 = {"Defense", 6, 20010, , , , 3};
+
+struct lightconestats lcid20011 = {"Loop", 4, 20011, , , , 3};
+
+struct lightconestats lcid20012 = {"Meshing Cogs", 5, 20012, , , , 3};
+
+struct lightconestats lcid20013 = {"Passkey", 2, 20013, , , , 3};
+
+struct lightconestats lcid20014 = {"Adversarial", 3, 20014, , , , 3};
+
+struct lightconestats lcid20015 = {"Multiplication", 7, 20015, , , , 3};
+
+struct lightconestats lcid20016 = {"Mutual Demise", 1, 20016, , , , 3};
+
+struct lightconestats lcid20017 = {"Pioneering", 6, 20017, , , , 3};
+
+struct lightconestats lcid20018 = {"Hidden Shadow", 4, 20018, , , , 3};
+
+struct lightconestats lcid20019 = {"Meditation", 5, 20019, , , , 3};
+
+struct lightconestats lcid20020 = {"Sagacity", 2, 20020, , , , 3};
+
+struct lightconestats lcid21000 = {"Post-Op Conversation", 7, 21000, , , , 4};
+
+struct lightconestats lcid21001 = {"Good Night and Sleep Well", 4, 21001, , , , 4};
+
+struct lightconestats lcid21002 = {"Day One of My New Life", 6, 21002, , , , 4};
+
+struct lightconestats lcid21003 = {"Only Silence Remains", 3, 21003, , , , 4};
+
+struct lightconestats lcid21004 = {"Memories of the Past", 5, 21004, , , , 4};
+
+struct lightconestats lcid21005 = {"The Moles Welcome You", 1, 21005, , , , 4};
+
+struct lightconestats lcid21006 = {"The Birth of the Self", 2, 21006, , , , 4};
+
+struct lightconestats lcid21007 = {"Shared Feeling", 5, 21007, , , , 4};
+
+struct lightconestats lcid21008 = {"Eyes of the Prey", 4, 21008, , , , 4};
+
+struct lightconestats lcid21009 = {"Landau's Choice", 6, 21009, , , , 4};
+
+struct lightconestats lcid21010 = {"Swordplay", 3, 21010, , , , 4};
+
+struct lightconestats lcid21011 = {"Planetary Rendezvous", 5, 21011, , , , 4};
+
+struct lightconestats lcid21012 = {"A secret Vow", 1, 21012, , , , 4};
+
+struct lightconestats lcid21013 = {"Make the World Clamor", 2, 21013, , , , 4};
+
+struct lightconestats lcid21014 = {"Perfect Timing", 7, 21014, , , , 4};
+
+struct lightconestats lcid21015 = {"Resolution Shines As Pearls of Sweat", 4, 21015, , , , 4};
+
+struct lightconestats lcid21016 = {"Trend of the Universal Market", 6, 21016, , , , 4};
+
+struct lightconestats lcid21017 = {"Subscribe for More!", 3, 21017, , , , 4};
+
+struct lightconestats lcid21018 = {"Dance! Dance! Dance!", 5, 21018, , , , 4};
+
+struct lightconestats lcid21019 = {"Under the Blue Sky", 1, 21019, , , , 4};
+
+struct lightconestats lcid21020 = {"Geniuses' Repose", 2, 21020, , , , 4};
+
+struct lightconestats lcid21021 = {"Quid Pro Quo", 7, 21021, , , , 4};
+
+struct lightconestats lcid21022 = {"Fermata", 4, 21022, , , , 4};
+
+struct lightconestats lcid21023 = {"We Are Wildfire", 6, 21023, , , , 4};
+
+struct lightconestats lcid21024 = {"River Flows in Spring", 3, 21024, , , , 4};
+
+struct lightconestats lcid21025 = {"Past and Future", 5, 21025, , , , 4};
+
+struct lightconestats lcid21026 = {"Woof! Walk Time!", 1, 21026, , , , 4};
+
+struct lightconestats lcid21027 = {"The Seriousness of Breakfast", 2, 21027, , , , 4};
+
+struct lightconestats lcid21028 = {"Warmth Shortens Cold Nights", 7, 21028, , , , 4};
+
+struct lightconestats lcid21029 = {"We Will Meet Again", 4, 21029, , , , 4};
+
+struct lightconestats lcid21030 = {"This Is Me!", 6, 21030, , , , 4};
+
+struct lightconestats lcid21031 = {"Return to Darkness", 3, 21031, , , , 4};
+
+struct lightconestats lcid21032 = {"Carve the Moon, Weave the Clouds", 5, 21032, , , , 4};
+
+struct lightconestats lcid21033 = {"Nowhere to Run", 1, 21033, , , , 4};
+
+struct lightconestats lcid21034 = {"Today Is Another Peaceful Day", 2, 21034, , , , 4};
+
+struct lightconestats lcid21035 = {"What Is Real?", 7, 21035, , , , 4};
+
+struct lightconestats lcid21036 = {"Dreamville Adventure", 5, 21036, , , , 4};
+
+struct lightconestats lcid21037 = {"Final Victor", 3, 21037, , , , 4};
+
+struct lightconestats lcid21038 = {"Flames Afar", 1, 21038, , , , 4};
+
+struct lightconestats lcid21039 = {"Destiny's Threads Forewoven", 6, 21039, , , , 4};
+
+struct lightconestats lcid21040 = {"The Day The Cosmos Fell", 2, 21040, , , , 4};
+
+struct lightconestats lcid21041 = {"It's Showtime", 4, 21041, , , , 4};
+
+struct lightconestats lcid21042 = {"Indelible Promise", 1, 21042, , , , 4};
+
+struct lightconestats lcid22000 = {"Before the Tutorial Mission Starts", 4, 22000, , , , 4};
+
+struct lightconestats lcid22001 = {"Hey, Over Here", 7, 22001, , , , 4};
+
+struct lightconestats lcid = {};
+
+struct lightconestats lcid = {};
+
+struct lightconestats lcid = {};
+
+struct lightconestats lcid = {};
+
+struct lightconestats lcid = {};
+
+struct lightconestats lcid = {};
+
+struct lightconestats lcid = {};
+
+struct lightconestats lcid = {};
+
+struct lightconestats lcid = {};
+
+struct lightconestats lcid = {};
+
+struct lightconestats lcid = {};
+
+struct lightconestats lcid = {};
+
+struct lightconestats lcid = {};
+
+struct lightconestats lcid = {};
+
+struct lightconestats lcid = {};
+
+struct lightconestats lcid = {};
+
+struct lightconestats lcid = {};
+
+struct lightconestats lcid = {};
+
+struct lightconestats lcid = {};
+
+struct lightconestats lcid = {};
+
+struct lightconestats lcid = {};
+
+struct lightconestats lcid = {};
+
+struct lightconestats lcid = {};
+
+struct lightconestats lcid = {};
+
+struct lightconestats lcid = {};
+
+struct lightconestats lcid = {};
+
+struct lightconestats lcid = {};
