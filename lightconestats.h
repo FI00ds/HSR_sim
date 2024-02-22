f14e11fa10f49755eaf5798c273ca556f1e4f657
FI00ds <lucasrobertson368@gmail.com>
2024-02-23 00:44:10 +0100

    updated to include all lightconestats as of version 2.0

--- lightconestats.h
+++ lightconestats.h
@@ -2,196 +2,196 @@ struct lightconestats{
     char* name;
     int path;
     int id;
-    int HP;
-    int ATK;
-    int DEF;
+    float HP;
+    float ATK;
+    float DEF;
     int rarity;
 };
 
 //Lightcone stats are for lvl 80 lightcones
 
-struct lightconestats lcid20000 = {"Arrows", 3, 20000, 846, 317, 264, 3};
+struct lightconestats lcid20000 = {"Arrows", 3, 20000, 846.72, 317.52, 264.60, 3};
 
-struct lightconestats lcid20001 = {"Cornucopia", 7, 20001, 952, 264, 264, 3};
+struct lightconestats lcid20001 = {"Cornucopia", 7, 20001, 952.56, 264.60, 264.60, 3};
 
-struct lightconestats lcid20002 = {"Collapsing Sky", 1, 20002, , , , 3};
+struct lightconestats lcid20002 = {"Collapsing Sky", 1, 20002, 846.72, 370.44, 198.45, 3};
 
-struct lightconestats lcid20003 = {"Amber", 6, 20003, , , , 3};
+struct lightconestats lcid20003 = {"Amber", 6, 20003, 846.72, 264.60, 330.75, 3};
 
-struct lightconestats lcid20004 = {"void", 4, 20004, , , , 3};
+struct lightconestats lcid20004 = {"void", 4, 20004, 846.72, 317.52, 264.60, 3};
 
-struct lightconestats lcid20005 = {"Chorus", 5, 20005, , , , 3};
+struct lightconestats lcid20005 = {"Chorus", 5, 20005, 846.72, 317.52, 264.60, 3};
 
-struct lightconestats lcid20006 = {"Data Bank", 2, 20006, , , , 3};
+struct lightconestats lcid20006 = {"Data Bank", 2, 20006, 740.88, 370.44, 264.60, 3};
 
-struct lightconestats lcid20007 = {"Darting Arrows", 3, 20007, , , , 3};
+struct lightconestats lcid20007 = {"Darting Arrows", 3, 20007, 740.88, 370.44, 264.60, 3};
 
-struct lightconestats lcid20008 = {"Fine Fruit", 7, 20008, , , , 3};
+struct lightconestats lcid20008 = {"Fine Fruit", 7, 20008, 952.56, 317.52, 198.45, 3};
 
-struct lightconestats lcid20009 = {"Shattered Home", 1, 20009, , , , 3};
+struct lightconestats lcid20009 = {"Shattered Home", 1, 20009, 846.72, 370.44, 198.45, 3};
 
-struct lightconestats lcid20010 = {"Defense", 6, 20010, , , , 3};
+struct lightconestats lcid20010 = {"Defense", 6, 20010, 952.56, 264.60, 264.60, 3};
 
-struct lightconestats lcid20011 = {"Loop", 4, 20011, , , , 3};
+struct lightconestats lcid20011 = {"Loop", 4, 20011, 846.72, 317.52, 264.60, 3};
 
-struct lightconestats lcid20012 = {"Meshing Cogs", 5, 20012, , , , 3};
+struct lightconestats lcid20012 = {"Meshing Cogs", 5, 20012, 846.72, 317.52, 264.60, 3};
 
-struct lightconestats lcid20013 = {"Passkey", 2, 20013, , , , 3};
+struct lightconestats lcid20013 = {"Passkey", 2, 20013, 740.88, 370.44, 264.60, 3};
 
-struct lightconestats lcid20014 = {"Adversarial", 3, 20014, , , , 3};
+struct lightconestats lcid20014 = {"Adversarial", 3, 20014, 740.88, 370.44, 264.60, 3};
 
-struct lightconestats lcid20015 = {"Multiplication", 7, 20015, , , , 3};
+struct lightconestats lcid20015 = {"Multiplication", 7, 20015, 952.56, 317.52, 198.45, 3};
 
-struct lightconestats lcid20016 = {"Mutual Demise", 1, 20016, , , , 3};
+struct lightconestats lcid20016 = {"Mutual Demise", 1, 20016, 846.72, 370.44, 198.45, 3};
 
-struct lightconestats lcid20017 = {"Pioneering", 6, 20017, , , , 3};
+struct lightconestats lcid20017 = {"Pioneering", 6, 20017, 952.56, 264.60, 264.60, 3};
 
-struct lightconestats lcid20018 = {"Hidden Shadow", 4, 20018, , , , 3};
+struct lightconestats lcid20018 = {"Hidden Shadow", 4, 20018, 846.72, 317.52, 264.60, 3};
 
-struct lightconestats lcid20019 = {"Meditation", 5, 20019, , , , 3};
+struct lightconestats lcid20019 = {"Meditation", 5, 20019, 846.72, 317.52, 264.60, 3};
 
-struct lightconestats lcid20020 = {"Sagacity", 2, 20020, , , , 3};
+struct lightconestats lcid20020 = {"Sagacity", 2, 20020, 740.88, 370.44, 264.60, 3};
 
-struct lightconestats lcid21000 = {"Post-Op Conversation", 7, 21000, , , , 4};
+struct lightconestats lcid21000 = {"Post-Op Conversation", 7, 21000, 1058.40, 423.36, 330.75, 4};
 
-struct lightconestats lcid21001 = {"Good Night and Sleep Well", 4, 21001, , , , 4};
+struct lightconestats lcid21001 = {"Good Night and Sleep Well", 4, 21001, 952.56, 476.28, 330.75, 4};
 
-struct lightconestats lcid21002 = {"Day One of My New Life", 6, 21002, , , , 4};
+struct lightconestats lcid21002 = {"Day One of My New Life", 6, 21002, 952.56, 370.44, 463.05, 4};
 
-struct lightconestats lcid21003 = {"Only Silence Remains", 3, 21003, , , , 4};
+struct lightconestats lcid21003 = {"Only Silence Remains", 3, 21003, 952.56, 476.28, 330.75, 4};
 
-struct lightconestats lcid21004 = {"Memories of the Past", 5, 21004, , , , 4};
+struct lightconestats lcid21004 = {"Memories of the Past", 5, 21004, 952.56, 423.36, 396.90, 4};
 
-struct lightconestats lcid21005 = {"The Moles Welcome You", 1, 21005, , , , 4};
+struct lightconestats lcid21005 = {"The Moles Welcome You", 1, 21005, 1058.40, 476.28, 264.60, 4};
 
-struct lightconestats lcid21006 = {"The Birth of the Self", 2, 21006, , , , 4};
+struct lightconestats lcid21006 = {"The Birth of the Self", 2, 21006, 952.56, 476.28, 330.75, 4};
 
-struct lightconestats lcid21007 = {"Shared Feeling", 5, 21007, , , , 4};
+struct lightconestats lcid21007 = {"Shared Feeling", 5, 21007, 952.56, 423.36, 396.90, 4};
 
-struct lightconestats lcid21008 = {"Eyes of the Prey", 4, 21008, , , , 4};
+struct lightconestats lcid21008 = {"Eyes of the Prey", 4, 21008, 952.56, 476.28, 330.75, 4};
 
-struct lightconestats lcid21009 = {"Landau's Choice", 6, 21009, , , , 4};
+struct lightconestats lcid21009 = {"Landau's Choice", 6, 21009, 952.56, 423.36, 396.90, 4};
 
-struct lightconestats lcid21010 = {"Swordplay", 3, 21010, , , , 4};
+struct lightconestats lcid21010 = {"Swordplay", 3, 21010, 952.56, 476.28, 330.75, 4};
 
-struct lightconestats lcid21011 = {"Planetary Rendezvous", 5, 21011, , , , 4};
+struct lightconestats lcid21011 = {"Planetary Rendezvous", 5, 21011, 1058.40, 423.36, 330.75, 4};
 
-struct lightconestats lcid21012 = {"A secret Vow", 1, 21012, , , , 4};
+struct lightconestats lcid21012 = {"A secret Vow", 1, 21012, 1058.40, 476.28, 264.60, 4};
 
-struct lightconestats lcid21013 = {"Make the World Clamor", 2, 21013, , , , 4};
+struct lightconestats lcid21013 = {"Make the World Clamor", 2, 21013, 846.72, 476.28, 396.90, 4};
 
-struct lightconestats lcid21014 = {"Perfect Timing", 7, 21014, , , , 4};
+struct lightconestats lcid21014 = {"Perfect Timing", 7, 21014, 952.56, 423.36, 396.90, 4};
 
-struct lightconestats lcid21015 = {"Resolution Shines As Pearls of Sweat", 4, 21015, , , , 4};
+struct lightconestats lcid21015 = {"Resolution Shines As Pearls of Sweat", 4, 21015, 952.56, 476.28, 330.75, 4};
 
-struct lightconestats lcid21016 = {"Trend of the Universal Market", 6, 21016, , , , 4};
+struct lightconestats lcid21016 = {"Trend of the Universal Market", 6, 21016, 1058.40, 370.44, 396.90, 4};
 
-struct lightconestats lcid21017 = {"Subscribe for More!", 3, 21017, , , , 4};
+struct lightconestats lcid21017 = {"Subscribe for More!", 3, 21017, 952.56, 476.28, 330.75, 4};
 
-struct lightconestats lcid21018 = {"Dance! Dance! Dance!", 5, 21018, , , , 4};
+struct lightconestats lcid21018 = {"Dance! Dance! Dance!", 5, 21018, 952.56, 423.36, 396.90, 4};
 
-struct lightconestats lcid21019 = {"Under the Blue Sky", 1, 21019, , , , 4};
+struct lightconestats lcid21019 = {"Under the Blue Sky", 1, 21019, 952.56, 476.28, 330.75, 4};
 
-struct lightconestats lcid21020 = {"Geniuses' Repose", 2, 21020, , , , 4};
+struct lightconestats lcid21020 = {"Geniuses' Repose", 2, 21020, 846.72, 476.28, 396.90, 4};
 
-struct lightconestats lcid21021 = {"Quid Pro Quo", 7, 21021, , , , 4};
+struct lightconestats lcid21021 = {"Quid Pro Quo", 7, 21021, 952.56, 423.36, 396.90, 4};
 
-struct lightconestats lcid21022 = {"Fermata", 4, 21022, , , , 4};
+struct lightconestats lcid21022 = {"Fermata", 4, 21022, 952.56, 476.28, 330.75, 4};
 
-struct lightconestats lcid21023 = {"We Are Wildfire", 6, 21023, , , , 4};
+struct lightconestats lcid21023 = {"We Are Wildfire", 6, 21023, 740.88, 476.28, 463.05, 4};
 
-struct lightconestats lcid21024 = {"River Flows in Spring", 3, 21024, , , , 4};
+struct lightconestats lcid21024 = {"River Flows in Spring", 3, 21024, 846.72, 476.28, 396.90, 4};
 
-struct lightconestats lcid21025 = {"Past and Future", 5, 21025, , , , 4};
+struct lightconestats lcid21025 = {"Past and Future", 5, 21025, 952.56, 423.36, 396.90, 4};
 
-struct lightconestats lcid21026 = {"Woof! Walk Time!", 1, 21026, , , , 4};
+struct lightconestats lcid21026 = {"Woof! Walk Time!", 1, 21026, 952.56, 476.28, 330.75, 4};
 
-struct lightconestats lcid21027 = {"The Seriousness of Breakfast", 2, 21027, , , , 4};
+struct lightconestats lcid21027 = {"The Seriousness of Breakfast", 2, 21027, 846.72, 476.28, 396.90, 4};
 
-struct lightconestats lcid21028 = {"Warmth Shortens Cold Nights", 7, 21028, , , , 4};
+struct lightconestats lcid21028 = {"Warmth Shortens Cold Nights", 7, 21028, 1058.40, 370.44, 396.90, 4};
 
-struct lightconestats lcid21029 = {"We Will Meet Again", 4, 21029, , , , 4};
+struct lightconestats lcid21029 = {"We Will Meet Again", 4, 21029, 846.72, 529.20, 330.75, 4};
 
-struct lightconestats lcid21030 = {"This Is Me!", 6, 21030, , , , 4};
+struct lightconestats lcid21030 = {"This Is Me!", 6, 21030, 846.72, 370.44, 529.20, 4};
 
-struct lightconestats lcid21031 = {"Return to Darkness", 3, 21031, , , , 4};
+struct lightconestats lcid21031 = {"Return to Darkness", 3, 21031, 846.72, 529.20, 330.75, 4};
 
-struct lightconestats lcid21032 = {"Carve the Moon, Weave the Clouds", 5, 21032, , , , 4};
+struct lightconestats lcid21032 = {"Carve the Moon, Weave the Clouds", 5, 21032, 952.56, 476.28, 330.75, 4};
 
-struct lightconestats lcid21033 = {"Nowhere to Run", 1, 21033, , , , 4};
+struct lightconestats lcid21033 = {"Nowhere to Run", 1, 21033, 952.56, 529.20, 264.60, 4};
 
-struct lightconestats lcid21034 = {"Today Is Another Peaceful Day", 2, 21034, , , , 4};
+struct lightconestats lcid21034 = {"Today Is Another Peaceful Day", 2, 21034, 846.72, 529.20, 330.75, 4};
 
-struct lightconestats lcid21035 = {"What Is Real?", 7, 21035, , , , 4};
+struct lightconestats lcid21035 = {"What Is Real?", 7, 21035, 1058.40, 423.36, 330.75, 4};
 
-struct lightconestats lcid21036 = {"Dreamville Adventure", 5, 21036, , , , 4};
+struct lightconestats lcid21036 = {"Dreamville Adventure", 5, 21036, 952.56, 423.36, 396.90, 4};
 
-struct lightconestats lcid21037 = {"Final Victor", 3, 21037, , , , 4};
+struct lightconestats lcid21037 = {"Final Victor", 3, 21037, 952.56, 476.28, 330.75, 4};
 
-struct lightconestats lcid21038 = {"Flames Afar", 1, 21038, , , , 4};
+struct lightconestats lcid21038 = {"Flames Afar", 1, 21038, 1058.40, 476.28, 264.60, 4};
 
-struct lightconestats lcid21039 = {"Destiny's Threads Forewoven", 6, 21039, , , , 4};
+struct lightconestats lcid21039 = {"Destiny's Threads Forewoven", 6, 21039, 952.56, 370.44, 463.05, 4};
 
-struct lightconestats lcid21040 = {"The Day The Cosmos Fell", 2, 21040, , , , 4};
+struct lightconestats lcid21040 = {"The Day The Cosmos Fell", 2, 21040, 952.56, 476.28, 330.75, 4};
 
-struct lightconestats lcid21041 = {"It's Showtime", 4, 21041, , , , 4};
+struct lightconestats lcid21041 = {"It's Showtime", 4, 21041, 1058.40, 476.28, 264.60, 4};
 
-struct lightconestats lcid21042 = {"Indelible Promise", 1, 21042, , , , 4};
+struct lightconestats lcid21042 = {"Indelible Promise", 1, 21042, 952.56, 476.28, 330.75, 4};
 
-struct lightconestats lcid22000 = {"Before the Tutorial Mission Starts", 4, 22000, , , , 4};
+struct lightconestats lcid22000 = {"Before the Tutorial Mission Starts", 4, 22000, 952.56, 476.28, 330.75, 4};
 
-struct lightconestats lcid22001 = {"Hey, Over Here", 7, 22001, , , , 4};
+struct lightconestats lcid22001 = {"Hey, Over Here", 7, 22001, 952.56, 423.36, 396.90, 4};
 
-struct lightconestats lcid = {};
+struct lightconestats lcid23000 = {"Night on the Milky Way", 2, 23000, 1164.24, 582.12, 396.90, 5};
 
-struct lightconestats lcid = {};
+struct lightconestats lcid23001 = {"In the Night", 3, 23001, 1058.40, 582.12, 463.05, 5};
 
-struct lightconestats lcid = {};
+struct lightconestats lcid23002 = {"Something Irreplaceable", 1, 23002, 1164.24, 582.12, 396.90, 5};
 
-struct lightconestats lcid = {};
+struct lightconestats lcid23003 = {"But the Battle Isn't Over", 5, 23003, 1164.24, 529.20, 463.05, 5};
 
-struct lightconestats lcid = {};
+struct lightconestats lcid23004 = {"In the Name of the World", 4, 23004, 1058.40, 582.12, 463.05, 5};
 
-struct lightconestats lcid = {};
+struct lightconestats lcid23005 = {"Moment of Victory", 6, 23005, 1058.40, 476.28, 595.35, 5};
 
-struct lightconestats lcid = {};
+struct lightconestats lcid23006 = {"Patience Is All You Need", 4, 23006, 1058.40, 582.12, 463.05, 5};
 
-struct lightconestats lcid = {};
+struct lightconestats lcid23007 = {"Incessant Rain", 4, 23007, 1058.40, 582.12, 463.05, 5};
 
-struct lightconestats lcid = {};
+struct lightconestats lcid23008 = {"Echoes of the Coffin", 7, 23008, 1164.24, 582.12, 396.90, 5};
 
-struct lightconestats lcid = {};
+struct lightconestats lcid23009 = {"The Unreachable Side", 1, 23009, 1270.08, 582.12, 330.75, 5};
 
-struct lightconestats lcid = {};
+struct lightconestats lcid23010 = {"Before Dawn", 2, 23010, 1058.40, 582.12, 463.05, 5};
 
-struct lightconestats lcid = {};
+struct lightconestats lcid23011 = {"She Already Shut Her Eyes", 6, 23011, 1270.08, 423.36, 529.20, 5};
 
-struct lightconestats lcid = {};
+struct lightconestats lcid23012 = {"Sleep Like the Dead", 3, 23012, 1058.40, 582.12, 463.05, 5};
 
-struct lightconestats lcid = {};
+struct lightconestats lcid23013 = {"Time Waits for No One", 7, 23013, 1270.08, 476.28, 463.05, 5};
 
-struct lightconestats lcid = {};
+struct lightconestats lcid23014 = {"I Shall Be My Own Sword", 1, 23014, 1164.24, 582.12, 396.90, 5};
 
-struct lightconestats lcid = {};
+struct lightconestats lcid23015 = {"Brighter Than the Sun", 1, 23015, 1058.40, 635.04, 396.90, 5};
 
-struct lightconestats lcid = {};
+struct lightconestats lcid23016 = {"Worrisome, Blissful", 3, 23016, 1058.40, 582.12, 463.05, 5};
 
-struct lightconestats lcid = {};
+struct lightconestats lcid23017 = {"Night of Fright", 7, 23017, 1164.24, 476.28, 529.20, 5};
 
-struct lightconestats lcid = {};
+struct lightconestats lcid23018 = {"An Instant Before A Gaze", 2, 23018, 1058.40, 582.12, 463.05, 5};
 
-struct lightconestats lcid = {};
+struct lightconestats lcid23019 = {"Past Self in Mirror", 5, 23019, 1058.40, 529.20, 529.20, 5};
 
-struct lightconestats lcid = {};
+struct lightconestats lcid23020 = {"Baptism of Pure Thought", 3, 23020, 952.56, 582.12, 529.20, 5};
 
-struct lightconestats lcid = {};
+struct lightconestats lcid23021 = {"Earthly Escapade", 5, 23021, 1164.24, 529.20, 463.05, 5};
 
-struct lightconestats lcid = {};
+struct lightconestats lcid23022 = {"Reforged Remembrance", 4, 23022, 1058.40, 582.12, 463.05, 5};
 
-struct lightconestats lcid = {};
+struct lightconestats lcid24000 = {"On the Fall of an Aeon", 1, 24000, 1058.40, 529.20, 396.90, 5};
 
-struct lightconestats lcid = {};
+struct lightconestats lcid24001 = {"Cruising in the Stellar Sea", 3, 24001, 952.56, 529.20, 463.05, 5};
 
-struct lightconestats lcid = {};
+struct lightconestats lcid24002 = {"Texture of Memories", 6, 24002, 1058.40, 423.36, 529.20, 5};
 
-struct lightconestats lcid = {};
+struct lightconestats lcid24003 = {"Solitary Healing", 4, 24003, 1058.40, 529.20, 396.90, 5};
