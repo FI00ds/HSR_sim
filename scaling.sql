c2d33de71b5802f0680d0a31a8a0029eecd331e6
FI00ds <lucasrobertson368@gmail.com>
2024-03-21 00:19:11 +0100

    template for adding levelling info

---
+++ scaling.sql
@@ -0,0 +1,52 @@
+UPDATE LCstats
+SET
+    "hp1+base" = 48 ,
+    "hp1+step" = 7.2 ,
+    "atk1+base" = 24 ,
+    "atk1+step" = 3.6 ,
+    "def1+base" = 18 ,
+    "def1+step" = 2.7 ,
+    
+    "hp20+base" = 105.6 ,
+    "hp20+step" = 7.2 ,
+    "atk20+base" = 52.8 ,
+    "atk20+step" = 3.6 ,
+    "def20+base" = 39.6 ,
+    "def20+step" = 2.7 ,
+    
+    "hp30+base" = 182.4 ,
+    "hp30+step" = 7.2 ,
+    "atk30+base" = 91.2 ,
+    "atk30+step" = 3.6 ,
+    "def30+base" = 68.4 ,
+    "def30+step" = 2.7 ,
+    
+    "hp40+base" = 259.2 ,
+    "hp40+step" = 7.2 ,
+    "atk40+base" = 129.6 ,
+    "atk40+step" = 3.6 ,
+    "def40+base" = 97.2 ,
+    "def40+step" = 2.7 ,
+
+    "hp50+base" = 336 ,
+    "hp50+step" = 7.2 ,
+    "atk50+base" = 168 ,
+    "atk50+step" = 3.6 ,
+    "def50+base" = 126 ,
+    "def50+step" = 2.7 ,
+    
+    "hp60+base" = 412.8 ,
+    "hp60+step" = 7.2 ,
+    "atk60+base" = 206.4 ,
+    "atk60+step" = 3.6 ,
+    "def60+base" = 154.8 ,
+    "def60+step" = 2.7 ,
+    
+    "hp70+base" = 489.6 ,
+    "hp70+step" = 7.2 ,
+    "atk70+base" = 244.8 ,
+    "atk70+step" = 3.6 ,
+    "def70+base" = 183.6 ,
+    "def70+step" = 2.7
+    
+WHERE "id" = 24003;
