b2ee756e3d53305c0b5f58893f1403716f240a9b
FI00ds <lucasrobertson368@gmail.com>
2024-03-25 02:31:02 +0100

    step now only appears once for each stat, began adding step/base added for characters

--- scaling.sql
+++ scaling.sql
@@ -1,52 +1,34 @@
-UPDATE LCstats
+UPDATE base_character
 SET
-    "hp1+base" = 48 ,
-    "hp1+step" = 7.2 ,
-    "atk1+base" = 24 ,
-    "atk1+step" = 3.6 ,
-    "def1+base" = 18 ,
-    "def1+step" = 2.7 ,
+    "hp1+base" = 124.8 ,
+    "atk1+base" = 76.8 ,
+    "def1+base" = 57 ,
     
-    "hp20+base" = 105.6 ,
-    "hp20+step" = 7.2 ,
-    "atk20+base" = 52.8 ,
-    "atk20+step" = 3.6 ,
-    "def20+base" = 39.6 ,
-    "def20+step" = 2.7 ,
+    "hp20+base" = 174.72 ,
+    "atk20+base" = 107.52 ,
+    "def20+base" = 79.8 ,
     
-    "hp30+base" = 182.4 ,
-    "hp30+step" = 7.2 ,
-    "atk30+base" = 91.2 ,
-    "atk30+step" = 3.6 ,
-    "def30+base" = 68.4 ,
-    "def30+step" = 2.7 ,
+    "hp30+base" = 224.64 ,
+    "atk30+base" = 138.24 ,
+    "def30+base" = 102.6 ,
     
-    "hp40+base" = 259.2 ,
-    "hp40+step" = 7.2 ,
-    "atk40+base" = 129.6 ,
-    "atk40+step" = 3.6 ,
-    "def40+base" = 97.2 ,
-    "def40+step" = 2.7 ,
+    "hp40+base" = 274.56 ,
+    "atk40+base" = 168.96 ,
+    "def40+base" = 125.4 ,
 
-    "hp50+base" = 336 ,
-    "hp50+step" = 7.2 ,
-    "atk50+base" = 168 ,
-    "atk50+step" = 3.6 ,
-    "def50+base" = 126 ,
-    "def50+step" = 2.7 ,
+    "hp50+base" = 324.48 ,
+    "atk50+base" = 199.68 ,
+    "def50+base" = 148.2 ,
     
-    "hp60+base" = 412.8 ,
-    "hp60+step" = 7.2 ,
-    "atk60+base" = 206.4 ,
-    "atk60+step" = 3.6 ,
-    "def60+base" = 154.8 ,
-    "def60+step" = 2.7 ,
+    "hp60+base" = 374.4 ,
+    "atk60+base" = 230.4 ,
+    "def60+base" = 171 ,
     
-    "hp70+base" = 489.6 ,
-    "hp70+step" = 7.2 ,
-    "atk70+base" = 244.8 ,
-    "atk70+step" = 3.6 ,
-    "def70+base" = 183.6 ,
-    "def70+step" = 2.7
+    "hp70+base" = 424.32 ,
+    "atk70+base" = 261.12 ,
+    "def70+base" = 193.8 ,
     
-WHERE "id" = 24003;
+    "hpstep" = 6.24 ,
+    "atkstep" = 3.84 ,
+    "defstep" = 2.85
+WHERE "id" = 1207;
